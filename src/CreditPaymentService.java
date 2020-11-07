c5287224d61218ee648c100a7bacb748f74d8d1c
Antonina Yazykova <adrianery94@>
2020-11-07 13:20:44 +0300

    java service commit

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class CreditPaymentService {
+public class CreditPaymentService {
+    public double calculate(double total, double percent, double period) {
+        double power = (percent / 1200) + 1;
+
+        double payment = total * (((percent / 1200) * (Math.pow(power, period)) / ((Math.pow(power, period) - 1))));
+
+        return payment;
+    }
 }
+
+
+
+
